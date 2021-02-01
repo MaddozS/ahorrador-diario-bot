7131fbd8efedd7d16ea5c1d6fc80a7152f0fb505
Axel Anaya <axel_anaya@hotmail.com>
2021-01-31 20:15:30 -0600

    connection to the db added

---
+++ modules/model/db.py
@@ -0,0 +1,28 @@
+from pymongo import MongoClient, database
+import os
+
+class DBInit():
+  def __init__(self, username, password, db_name):
+    self.username = username
+    self.password = password
+    self.database_name = db_name
+
+
+  def start_connection(self):
+    uri = os.environ['db_uri']
+
+    uri = uri.replace('<user>', self.username)
+    uri = uri.replace('<pass>', self.password)
+    uri = uri.replace('<dbname>', self.database_name)
+    
+    self.client = MongoClient(uri)
+
+  def get_database(self):
+    return self.client[self.database_name]
+
+# client = MongoClient('mongodb+srv://admin:rZ5PlXCJuGkyw9i0@cluster0.8oilk.mongodb.net/ahorrador_diario?retryWrites=true&w=majority')
+
+database = DBInit(os.environ['username_db'], os.environ['password_db'], os.environ['name_db'])
+database.start_connection()
+
+DB = database.get_database()
